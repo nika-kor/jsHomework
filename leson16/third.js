73e73ec48fafa9bfd593e3062690005d6353e121
Nika <nika.koromyslova@gmail.com>
2024-12-22 20:06:44 +0200

    leson 16

---
+++ leson16/third.js
@@ -0,0 +1,64 @@
+console.log(`Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. При виклику буде отримувати один аргумент - початковий масив товарів, і записувати його у властивість items.
+
+
+
+Додай методи класу:
+
+
+
+getItems() — повертає масив поточних товарів
+addItem(item) — отримує новий товар і додає його до поточних
+removeItem(item) — отримує товар і, якщо він є, видаляє його з поточних`)
+
+
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+    getItems() {
+        return this.items;
+    }
+    addItem(item) {
+        this.items.push(item);
+
+    }
+    removeItem(item) {
+        const index = this.items.indexOf(item);
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
+    }
+}
+const storage = new Storage([
+    'Нанітоіди',
+    'Пролонгер',
+    'Залізні жупи',
+    'Антигравітатор',
+   ]);
+   
+   const items = storage.getItems();
+   console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+   
+   storage.addItem('Дроїд');
+   console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+   
+   storage.removeItem('Пролонгер');
+   console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
